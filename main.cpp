68d9f45c41609c41f96836ebc658fcb119fc8ada
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-06-08 10:37:28 +0200

    Music added

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
 #include <stdint.h>
 #include "HeaderFiles/Player.h"
 #include "HeaderFiles/Ball.h"
@@ -7,24 +10,73 @@
 #include "HeaderFiles/Timer.h"
 #include "HeaderFiles/Collisions.h"
 #include "HeaderFiles/Globals.h"
+#include "HeaderFiles/Texture.h"
 
 // Global variables
 SDL_Window* globalWindow = nullptr;
 SDL_Renderer* globalRenderer = nullptr;
+Mix_Music* globalMusic = nullptr;
 
 // Initialize SDL and the game window
 bool init() {
+    // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
         return false;
     }
 
-    globalWindow = SDL_CreateWindow("Arkanoid", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (globalWindow == nullptr) {
+    // Set texture filtering to linear
+    if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+        printf("Warning: Linear texture filtering not enabled!");
+    }
+
+    // Initialize SDL_ttf
+    if (TTF_Init() == -1) {
+        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
         return false;
     }
 
+    Uint32 windowFlags = SDL_WindowFlags::SDL_WINDOW_SHOWN;
+
+    // Create window
+    globalWindow = SDL_CreateWindow("MGD Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, windowFlags);
+    if (globalWindow == NULL) {
+        printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    // Create renderer for window
     globalRenderer = SDL_CreateRenderer(globalWindow, -1, SDL_RENDERER_ACCELERATED);
-    if (globalRenderer == nullptr) {
+    if (globalRenderer == NULL) {
+        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+        return false;
+    }
+
+    // Initialize SDL_mixer
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+        return false;
+    }
+    // Load music
+    globalMusic = Mix_LoadMUS("Assets/BG_Music.mp3");
+    if (globalMusic == NULL) {
+        printf("Failed to load music! SDL_mixer Error: %s\n", Mix_GetError());
+        return false;
+    }
+    if (Mix_PlayMusic(globalMusic, -1) == -1) {
+        printf("Failed to play music! SDL_mixer Error: %s\n", Mix_GetError());
+        return false;
+    }
+    // Initialize the renderer blend mode
+    SDL_SetRenderDrawBlendMode(globalRenderer, SDL_BlendMode::SDL_BLENDMODE_BLEND);
+
+    // Initialize renderer color
+    SDL_SetRenderDrawColor(globalRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+    // Initialize PNG loading
+    int imgFlags = IMG_INIT_PNG;
+    if (!(IMG_Init(imgFlags) & imgFlags)) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
         return false;
     }
 
@@ -138,6 +190,9 @@ void render(Player& player, Ball& ball, Block levels [][NUM_BLOCKS]) {
 // Main function of the game
 void runGame() {
     
+    //Background creation and rendering
+    Texture* backgroundTexture = new Texture("Assets/Arkanoid_Background.png");
+    
     //Player definition
     float player_X = SCREEN_WIDTH / 2 - PLAYER_WIDTH / 2;
     float player_Y = SCREEN_HEIGHT - PLAYER_HEIGHT - 10;
@@ -164,10 +219,15 @@ void runGame() {
         quit = update(*player, *ball, levels);
         
         render(*player, *ball, levels);
+
+        SDL_Rect rectDest = { 0, 0, 800, 600 };
+        //backgroundTexture->renderCopyEx(&rectDest);
+        backgroundTexture->render();
     }
 
-    delete(player);
-    delete(ball);
+    delete player;
+    delete ball;
+    delete backgroundTexture;
 }
 
 // Free resources and close game
@@ -178,6 +238,8 @@ void close() {
 }
 
 int main(int argc, char* args[]) {
+    
+
     if (!init()) {
         return -1;
     }
